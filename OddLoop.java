efd375766395d476b39ef8c5352c4bc6f746c310
NICHOLAS ICHUKWU <ichukwunicholasagbo@gmail.com>
2024-12-07 07:37:15 +0100

    add OddLoop.java
    git commit -m add

---
+++ OddLoop.java
@@ -0,0 +1,19 @@
+public class OddLoop{
+
+	public static void main(String[] args){
+
+        
+ 
+         for(int count = 1; count <= 10; count++){
+
+         if(count %3 == 0){
+
+        System.out.println(count);
+     
+          }
+
+        }
+
+     }
+
+ }
