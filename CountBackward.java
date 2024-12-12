a00708fb933f6671a1873eb02bf5a551b21c9101
NICHOLAS ICHUKWU <ichukwunicholasagbo@gmail.com>
2024-12-12 01:39:07 +0100

    add countBar

--- CountBackward.java
+++ CountBackward.java
@@ -2,7 +2,7 @@ public class CountBackward{
 
 	public static void main(String[] args){
 
-	for(int i = 19; 1 >= 1; i -= 3){
+	for(int i = 19; 1 >= 0; i -= 2){
         if(i < 1) break;
 	System.out.println(i);
 	}
