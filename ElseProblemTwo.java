dfb19e8b011031c013a56c25b7236e7e31186ab9
NICHOLAS ICHUKWU <ichukwunicholasagbo@gmail.com>
2024-12-13 17:27:27 +0100

    add ElsePormblemOne

---
+++ ElseProblemTwo.java
@@ -0,0 +1,25 @@
+public class ElseProblemTwo{
+
+	public static void main(String[] args){
+
+	int x = 5;
+
+	int y = 5;
+
+	if(x < 10){
+
+	if(y > 10){
+	System.out.println("*****");
+	} else {
+	System.out.println("#####");
+	System.out.println("$$$$$");
+	 }
+      
+        }
+
+
+
+      }
+
+
+   }
